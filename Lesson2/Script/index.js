1165cb17be114426604fe816364e3f5c4fd9f591
VahagMyan <59207741+VahagMyan@users.noreply.github.com>
2020-01-24 22:47:44 +0400

    Constructor

---
+++ Lesson2/Script/index.js
@@ -0,0 +1,183 @@
+// var nums = [-1, 300, 12, 110, -901];
+
+// for (var i = 0; i < nums.length; i++){
+//     for (var j = 0; j < nums.length; j++){
+//         if (nums[i] < nums[j]){
+//             var container = nums[i];
+//             nums[i] = nums[j];
+//             nums[j] = container;
+//         }
+//     }
+// }
+
+// console.log(nums);
+
+
+// var f = function (){
+//     alert('test');
+// }
+
+// f();
+
+
+// f();
+
+// var f = function (){
+//     alert();
+// }
+
+
+// function f (){
+//     console.log(arguments[0]);
+// }
+
+// f(4,50);
+
+
+// function f (a, b, c){
+//     console.log(f.length);
+// }
+// f();
+
+
+// var human = new Object();
+// human.name = 'Exo'; 
+// human.age = 30;
+
+// human.sayName = function(){
+//     alert(this.name);
+// }
+
+// console.log(typeof human);
+
+// human.sayName();
+
+
+// var human = {
+//     name: 'Exo',
+//     age: 30,
+//     sayName: function (){
+//         alert(this.name);
+//     }
+// }
+
+// human.sayName();
+// console.log(human);
+// human.lastName = 'Exoyan';
+// human.age = 31;
+
+
+// var arr = [];
+
+// function f() {
+// }
+// console.log(f instanceof Object);
+// console.log(arr instanceof Object);
+
+
+// var obj = {
+//     age:30
+// }
+
+// function f(){
+//     console.log(this.age);
+// }
+
+// f();
+
+// obj.sayAge = f;
+// obj.sayAge();
+
+
+// console.log(this);
+
+
+// var a = 10;
+
+// console.log(window.a);
+
+
+// this.a;
+// window === this;
+
+// console.log(window === this);
+
+
+// var obj = {
+//     age:10,
+//     sayAge: function () { f();}
+// }
+
+// function f (){
+//     console.log(this.age);
+// }
+
+// this.age = 100;
+// obj.sayAge();
+
+
+// function Person(a, b,c){
+//     this.firstName = a;
+//     this.lastName = b;
+//     this.age = c;
+//     this.sayFullName = function(){
+//         console.log(`My name is ${this.firstName} ${this.lastName}`);
+//     }
+// }
+
+// var human0 = new Person('Exo', 'Exoyan', 40);
+// human0.sayFullName();
+// console.log(human0.age);
+// var human1 = new Person('Vazgen','Matevosyan', 50);
+// human1.sayFullName();
+// console.log(human1.age);
+
+
+// var result = 0;
+
+// function f() {
+//     for (var i = 0; i < result.length, i++){
+//         if(result > result[i]){
+
+//         }       
+//     }
+// }
+
+// f(10, -30, 4);
+// console.log(result);
+
+
+// console.log(0B10);
+// console.log(0b10);
+// console.log(0O10);
+// console.log(0o10);
+// console.log(0X10);
+// console.log(0x10);
+
+
+// console.log(typeof true.toString());
+
+// var x = 5;
+
+// console.log(x.toString());
+// console.log(5..toString());
+
+
+
+
+
+getMax(5, -10, 200, 3);
+ete mek tiv grvi, alert ta 2 tiv piti grvi
+gtnel maximal arjeq@
+ete baci tvic ayyl arjeq lini, alert!
+NaN isNaN(Nan);
+Infinity === Infinity
+true
+isFinite(10);
+true
+getMax(5);
+
+sarqel konstruktor erb var obj = new F();
+F(); ete kanchenq arandzin aranc new-i kanchi alert!
+
+sortavorman xndir
